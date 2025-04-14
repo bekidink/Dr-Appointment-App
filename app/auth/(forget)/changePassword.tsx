82c405336fcea4fac374e14de837386b75928eff
bekidink <bereketdinku876@gmail.com>
2025-04-14 00:29:39 -0700

    change password

---
+++ app/auth/(forget)/changePassword.tsx
@@ -0,0 +1,80 @@
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { AppIcon } from '~/constants/images';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { InputWithIcon } from '~/components/inputs/InputWithIcon';
+import CustomButton from '~/components/CustomButton';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { Stack, useRouter } from 'expo-router';
+
+const schema = z.object({
+  newPassword: z.string().email('Please enter a valid email'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const ChangePassword = () => {
+  const navigation = useNavigation();
+  const router = useRouter();
+  const {
+    handleSubmit,
+    setValue,
+    watch,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      newPassword: '',
+      password: '',
+    },
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log('Submitted', data);
+    router.replace('/auth/profile-onboarding');
+  };
+
+  return (
+    <View className="flex-1 bg-white">
+      <Stack.Screen options={{ headerShown: true, title: '' }} />
+      {/* Logo & Titles */}
+      <View className="mt-16 items-center justify-center">
+        <Image source={AppIcon} style={{ width: 50, height: 50 }} resizeMode="contain" />
+        <Text className="mt-3 text-2xl font-bold text-[#6B7280]">HealthPal</Text>
+        <Text className="mt-5 text-2xl font-bold text-[#1C2A3A]">Create new password </Text>
+        <Text className="mt-2 text-base font-medium text-[#6B7280]">
+          Your new password must be different form previously used password
+        </Text>
+      </View>
+
+      {/* Form */}
+      <View className="mx-5 mt-5">
+        <InputWithIcon
+          icon="key"
+          placeholder="Password"
+          value={watch('password')}
+          onChangeText={(text) => setValue('password', text, { shouldValidate: true })}
+          error={errors.password?.message}
+        />
+        <InputWithIcon
+          icon="key"
+          placeholder="Confirm Password"
+          value={watch('newPassword')}
+          onChangeText={(text) => setValue('newPassword', text, { shouldValidate: true })}
+          error={errors.newPassword?.message}
+        />
+        <CustomButton
+          title="Reset Password"
+          onPress={handleSubmit(onSubmit)}
+          className="mt-4 rounded-full bg-[#111928] py-3"
+        />
+      </View>
+    </View>
+  );
+};
+
+export default ChangePassword;
---
+++ app/auth/(forget)/verify.tsx
@@ -0,0 +1,92 @@
+import React, { useState, useRef } from 'react';
+import { View, Text, TouchableOpacity, Platform, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
+import { CodeField, Cursor } from 'react-native-confirmation-code-field';
+import { TextInput } from 'react-native';
+import { AppIcon } from '~/constants/images';
+import { Image } from 'react-native';
+
+const OtpVerificationScreen = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [otp, setOtp] = useState('');
+  const router = useRouter();
+  const ref = useRef(null);
+
+  const CELL_COUNT = 6; // Define the number of OTP digits
+
+  const handleVerifyOtp = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+      console.log('OTP Verified:', otp);
+      // Navigate to the next screen upon successful OTP verification
+      //   router.push('/next-screen'); // Replace with your actual path
+    }, 2000);
+  };
+
+  return (
+    <View className="flex-1  bg-white p-5">
+      <View className="mt-16 items-center justify-center">
+        <Image source={AppIcon} style={{ width: 50, height: 50 }} resizeMode="contain" />
+        <Text className="mt-3 text-2xl font-bold text-[#6B7280]">HealthPal</Text>
+        <Text className="mt-5 text-2xl font-bold text-[#1C2A3A]">Verify Code </Text>
+        <Text className="mt-2 text-base font-medium text-[#6B7280]">
+          Enter the the code we just sent you on your registered Email
+        </Text>
+      </View>
+
+      <View className="mt-5 w-full">
+        {/* OTP CodeField */}
+        <CodeField
+          ref={ref}
+          value={otp}
+          onChangeText={setOtp}
+          cellCount={CELL_COUNT}
+          keyboardType="number-pad"
+          textContentType="oneTimeCode"
+          //   autoComplete={Platform.select({ android: 'sms-otp', default: 'one-time-code' })}
+          testID="otp-input"
+          rootStyle={{ justifyContent: 'space-between', flexDirection: 'row', width: '100%' }}
+          InputComponent={TextInput} // Ensure to provide the InputComponent
+          renderCell={({ index, symbol, isFocused }) => (
+            <Text
+              key={index}
+              style={[
+                {
+                  width: 45,
+                  height: 45,
+                  borderWidth: 2,
+                  borderColor: '#ccc',
+                  borderRadius: 10,
+                  textAlign: 'center',
+                  fontSize: 18,
+                },
+                isFocused && { borderColor: '#111928' }, // Change border color when focused
+              ]}>
+              {symbol || (isFocused ? <Cursor /> : null)}
+            </Text>
+          )}
+        />
+      </View>
+
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#111928" className="mt-5" />
+      ) : (
+        <TouchableOpacity
+          onPress={handleVerifyOtp}
+          className="mt-5 rounded-full bg-[#111928] px-6 py-3">
+          <Text className="text-center text-white">Verify</Text>
+        </TouchableOpacity>
+      )}
+
+      <Text className="mt-4 text-center text-sm text-[#6B7280]">
+        Didn't receive the code?{' '}
+        <TouchableOpacity>
+          <Text className="text-blue-500">Resend</Text>
+        </TouchableOpacity>
+      </Text>
+    </View>
+  );
+};
+
+export default OtpVerificationScreen;
